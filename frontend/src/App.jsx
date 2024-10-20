e84ac6cc7a6101ecab9d9b4cb86f230c418564bc
esraelif <elifesratunca@gmail.com>
2024-10-20 16:25:30 +0200

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+import { Outlet } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <nav>Navbar</nav>
+      <Outlet />
+      <footer>Footer</footer>
+    </>
+  )
+}
+
+export default App
