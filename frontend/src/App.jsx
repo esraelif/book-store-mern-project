994c0ec02fc145d04a36a23cf5d2c8cdc5869b56
esraelif <elifesratunca@gmail.com>
2024-10-26 14:00:17 +0200

    "Updated cSpell words, added Footer component, removed favicon and footer logo"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
 
@@ -13,7 +14,7 @@ function App() {
         <Outlet />
       </main>
 
-      <footer>Footer</footer>
+      <Footer />
     </>
   )
 }
